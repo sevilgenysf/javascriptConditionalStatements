aca22f6cbd01cb42b8101991cc9cd061c56c8301
Yusuf Sevilgen <yusufsevilgenonline@gmail.com>
2021-09-22 08:31:29 +0300

    first commit

---
+++ person.js
@@ -0,0 +1,10 @@
+var person;
+person = 18;
+
+if (person < 18) {
+  console.log("kafeye giremezsin");
+} else if (person > 18 || 18 && person < 23) {
+  console.log("Kafeye girebilir ama Alkol alamaz.");
+} else if (person > 23) {
+  console.log("Kafeye girebilir ve alkol alabilirsin");
+}
