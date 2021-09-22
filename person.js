b3b13f49dfc85b5266defe1bad518268f1df0ff3
Yusuf Sevilgen <yusufsevilgenonline@gmail.com>
2021-09-22 12:40:04 +0300

    conditionalStatements

--- person.js
+++ person.js
@@ -1,10 +1,10 @@
 var person;
-person = 18;
+person = 27;
 
 if (person < 18) {
   console.log("kafeye giremezsin");
-} else if (person > 18 || 18 && person < 23) {
+} else if (person > 18 && 18 , person < 23) {
   console.log("Kafeye girebilir ama Nargile alamaz.");
-} else if (person > 23) {
+} else if (person > 23 || 23) {
   console.log("Kafeye girebilir ve Nargile alabilirsin");
 }
